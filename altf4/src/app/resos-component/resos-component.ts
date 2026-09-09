7a3b72730166de0ec9a04e2339b77efad0553c23
Resos-JohnAlec <johnalecresos@gmail.com>
2026-09-09 10:40:46 +0800

    Complete step 5, add html structure and style

--- altf4/src/app/resos-component/resos-component.ts
+++ altf4/src/app/resos-component/resos-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-resos-component',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './resos-component.html',
   styleUrl: './resos-component.scss',
 })

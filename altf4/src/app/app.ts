1b7735b7357a93c73e62017e975098e706207922
Revillas <eunicegrace.revs@gmail.com>
2026-09-09 12:23:58 +0800

    create revillas component and add into root app

--- altf4/src/app/app.ts
+++ altf4/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RevillasComponent} from './revillas-component/revillas-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RevillasComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ altf4/src/app/revillas-component/revillas-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-revillas-component',
+  imports: [],
+  templateUrl: './revillas-component.html',
+  styleUrl: './revillas-component.scss',
+})
+export class RevillasComponent {}

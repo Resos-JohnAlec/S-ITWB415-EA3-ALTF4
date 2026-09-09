266d3f98d00e1f419b1e98b0c1beab24ad343233
lauron, pat <pjameslauron@gmail.com>
2026-09-09 12:37:39 +0800

    add lauron-component

---
+++ altf4/src/app/lauron-component/lauron-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lauron-component',
+  imports: [],
+  templateUrl: './lauron-component.html',
+  styleUrl: './lauron-component.scss',
+})
+export class LauronComponent {}

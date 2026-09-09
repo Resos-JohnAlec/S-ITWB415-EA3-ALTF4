55c79bce5b0ef376e5c0a8e2da977db919958d0e
Revillas <eunicegrace.revs@gmail.com>
2026-09-09 13:08:18 +0800

    Apply Material button

--- altf4/src/app/revillas-component/revillas-component.ts
+++ altf4/src/app/revillas-component/revillas-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-revillas-component',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './revillas-component.html',
   styleUrl: './revillas-component.scss',
 })

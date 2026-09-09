5014b7ff7d130fd83f46fead0d65691ac7aa14dc
Magno-BiancaLauryn <biancamagno09@gmail.com>
2026-09-09 11:32:56 +0800

    feat: create magno component

---
+++ altf4/src/app/components/magno-component/magno-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-magno-component',
+  imports: [],
+  templateUrl: './magno-component.html',
+  styleUrl: './magno-component.scss',
+})
+export class MagnoComponent {}

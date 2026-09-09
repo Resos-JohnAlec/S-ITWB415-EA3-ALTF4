8982a108b4e1937822e1bde8ea3d967a9c6bff93
Revillas <eunicegrace.revs@gmail.com>
2026-09-09 13:32:30 +0800

    feat1 Revillas: Step 1

--- altf4/src/app/app.ts
+++ altf4/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ResosComponent } from './resos-component/resos-component';
 import { MagnoComponent } from './components/magno-component/magno-component';
+import { RevillasComponent } from './revillas-component/revillas-component';  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ResosComponent, MagnoComponent],
+  imports: [RouterOutlet, ResosComponent, MagnoComponent, RevillasComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

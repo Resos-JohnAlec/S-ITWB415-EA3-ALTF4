5cdd5d8bf480b928cc9799bfc99275ad9d8742dc
lauron, pat <pjameslauron@gmail.com>
2026-09-15 21:40:00 +0800

    Use memberPreview component  to LauronComponent

--- altf4/src/app/components/lauron-component/lauron-component.ts
+++ altf4/src/app/components/lauron-component/lauron-component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, signal } from '@angular/core';
+import { MemberPreview } from '../../shared/member-preview/member-preview';
 
 @Component({
-  imports: [MatButtonModule],
+  imports: [MemberPreview],
   selector: 'app-lauron-component',
   styleUrl: './lauron-component.scss',
   templateUrl: './lauron-component.html',
 })
 export class LauronComponent {
-  fullname = 'Patrick James S. Lauron';
-  photo = '/images/lauron.jpg';
-  description = '';
-
-  showDescription() {
-    this.description =
-      'I am an IT student passionate about building things for the web. ' + 
-      'I may not know everything yet, but I show up ready to learn.';
-  }
+  name = signal('Patrick James S. Lauron');
+  image = signal('./images/lauron.jpg');
+  role = signal('IT student · Web development');
+  description = signal(
+    'I am an IT student passionate about building things for the web. ' +
+      'I may not know everything yet, but I show up ready to learn.',
+  );
+  github = signal(`https://github.com/Lauron-PatrickJames`);
 }

b144db0d76796cd928f2c1fd61cc4a5321de4a73
Magno-BiancaLauryn <biancamagno09@gmail.com>
2026-09-15 13:05:34 +0800

    feat: use MemberPreview component in MagnoComponent

--- altf4/src/app/components/magno-component/magno-component.ts
+++ altf4/src/app/components/magno-component/magno-component.ts
@@ -1,20 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { MemberPreview } from '../../shared/member-preview/member-preview';
 
 @Component({
   selector: 'app-magno-component',
-  imports: [MatButtonModule],
+  imports: [MemberPreview],
   templateUrl: './magno-component.html',
   styleUrl: './magno-component.scss',
 })
 export class MagnoComponent {
-  name = 'Bianca H. Magno';
-  photo = `images/magno.jpg`;
-  description = signal('');
-
-  showDescription() {
-    this.description.set(
-      'I am an IT student who enjoys designing and creating UI and UX.'
-    );
-  }
+  name = signal('Bianca Lauryn H. Magno');
+  image = signal('images/magno.jpg');
+  role = signal('IT student · Web development');
+  description = signal(
+    'I am an IT student who enjoys designing and creating UI and UX.',
+  );
+  github = signal('https://github.com/MagnoBiancaLauryn');
 }
